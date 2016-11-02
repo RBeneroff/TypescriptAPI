6b6eba28e3595cc95d463a1f897ff3365230eb61
Adam Youngblood <ayoungbl0d@gmail.com>
2016-11-02 12:19:37 -0500

    userscontroller work

--- backendapi/config/routes.rb
+++ backendapi/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:create, :show, :destroy] do
+  resources :users, only: [:create, :show, :destroy, :index] do
     collection do
       post '/login', to: 'users#login'
     end
